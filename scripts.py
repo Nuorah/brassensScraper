b1edeb125539133d68165e8b3aaa00ae06ea990d
Haroun Moatassime <haroun.moatassime@gmail.com>
2019-03-12 14:25:45 +0100

    Deleted commented dead code

--- scripts.py
+++ scripts.py
@@ -6,9 +6,5 @@ url = "http://www.brassens-cahierdechanson.fr/OEUVRES/CHANSONS/ombremaris.html"
 
 page = urlopen(url)
 soup = BeautifulSoup(page, 'html.parser')
-#print(soup.prettify())
 text_by_lines = []
 print(soup.find_all('td', attrs={'class': 'titre'})[0].text)
-
-
-#print(soup.prettify())
